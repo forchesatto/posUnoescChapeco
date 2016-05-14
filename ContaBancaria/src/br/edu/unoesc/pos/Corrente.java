a2c1440911ebf81dad32a7fd75f0f6a492d43398
andre.forchesatto <andreforchesatto@gmail.com>
2016-05-14 09:23:18 -0300

    Extend conta

---
+++ ContaBancaria/src/br/edu/unoesc/pos/Corrente.java
@@ -0,0 +1,14 @@
+package br.edu.unoesc.pos;
+
+public class Corrente extends Conta {
+
+	private Double limite;
+
+	public Double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(Double limite) {
+		this.limite = limite;
+	}
+}
---
+++ ContaBancaria/src/br/edu/unoesc/pos/Poupanca.java
@@ -0,0 +1,14 @@
+package br.edu.unoesc.pos;
+
+public class Poupanca extends Conta {
+
+	private Double rendimento;
+
+	public Double getRendimento() {
+		return rendimento;
+	}
+
+	public void setRendimento(Double rendimento) {
+		this.rendimento = rendimento;
+	}
+}
